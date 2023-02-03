aec125c7229a67b6289bfa669ad8b01b8a0bf52a
Aksel <jacobaxelth@outlook.com>
2023-02-02 20:58:44 -0600

    Update website title

--- php/websites.php
+++ php/websites.php
@@ -63,7 +63,7 @@ $websites = $main_conn->query($websites_query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <script src="../js/main.js" defer="defer" type="module"></script>
-    <title>Websites</title>
+    <title>Websites | Admin</title>
 </head>
 <body>
 
