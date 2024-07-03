6f0551997a506292b4de0dc20dd01ef766df6b42
Axel Jacobo <jacobaxelth@outlook.com>
2024-07-03 16:11:51 -0600

    Fix adding multiple scale() values
    Extract the translate() value from the transform string instead of
    scale() and replace all of the 'transform' property content with the
    saved translate() bit plus the new scale value.
    
    See: #27

--- js/classes/zoomableImage.js
+++ js/classes/zoomableImage.js
@@ -13,20 +13,24 @@ export class zoomableImage {
     zoomIn() {
         if ( this.actualScale < this.maxScale ) {
             this.actualScale += 0.1;
-            let scaleLessTransformString = this.node.style.transform.replace(/scale\(\d\.\d\)\s?/g, '');
-            console.log(scaleLessTransformString)
-            let newTransformString = `${ scaleLessTransformString } scale(${ this.actualScale })`;
-            this.node.style.transform = newTransformString;
-            // console.warn( this.node.getBoundingClientRect() );
+            let translateStringRegExp = /translate\(-?\d{1,}px, -?\d{1,}px\)/ig;
+            let translateString = translateStringRegExp.exec(this.node.style.transform);
+            let transformString = ( translateString ) 
+                                  ? `${ translateString[0] } scale(${ this.actualScale })`
+                                  : `translate(0px, 0px) scale(${ this.actualScale })`;
+            this.node.style.transform = transformString;
         }
     };
 
     zoomOut() {
         if ( this.actualScale > this.minScale ) {
             this.actualScale -= 0.1;
-            let scaleLessTransformString = this.node.style.transform.replace(/scale\(\d\.\d\)\s?/g, '');
-            let newTransformString = `${ scaleLessTransformString } scale(${ this.actualScale })`;
-            this.node.style.transform = newTransformString;
+            let translateStringRegExp = /translate\(-?\d{1,}px, -?\d{1,}px\)/ig;
+            let translateString = translateStringRegExp.exec(this.node.style.transform);
+            let transformString = ( translateString ) 
+                                  ? `${ translateString[0] } scale(${ this.actualScale })`
+                                  : `translate(0px, 0px) scale(${ this.actualScale })`;
+            this.node.style.transform = transformString;
         }
     }
 
