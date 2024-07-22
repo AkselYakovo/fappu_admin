2c40150dac40b063ece67c72b3808af99b914e1a
Axel Jacobo <jacobaxelth@outlook.com>
2024-07-22 15:13:35 -0600

    Create Scroller Class
    Scroller class is used within the custom element select-emphasized
    for the scrolling behaviour of the options list. It currently
    supports only the vertical scroll behaviour on desktop devices.
    
    See: #31

---
+++ js/classes/Scroller.js
@@ -0,0 +1,39 @@
+export class Scroller {
+
+    constructor(node, options) {
+        this.node = node;
+        this.parent = node.parentElement;
+        this.parent.coords = this.parent.getBoundingClientRect();
+        this.visibleArea = options.visibleArea ?? { X:this.parent.coords.width, Y: this.parent.coords.height};
+        this.stepSize = options.stepSize;
+        this.currentStep = 0;
+        this.maxStep = { 
+            X: Math.round( (this.node.offsetHeight - this.visibleArea.X) / this.stepSize),
+            Y: Math.round( (this.node.offsetHeight - this.visibleArea.Y) / this.stepSize),
+        }
+
+        node.onwheel = (e) => { 
+            e.preventDefault();
+            if ( e.deltaY > 0 ) {
+                this.scrollDown();
+            }
+            else {
+                this.scrollUp();
+            }
+        }
+    }
+
+    scrollDown() {
+        if ( Math.abs(this.currentStep) < this.maxStep.Y ) {
+            this.currentStep--;
+            this.node.style.transform = `translateY(${ this.currentStep * this.stepSize }px)`;
+        }
+    }
+
+    scrollUp() {
+        if ( this.currentStep < 0 ) {
+            this.currentStep++;
+            this.node.style.transform = `translateY(${ this.currentStep * this.stepSize }px)`;
+        }
+    }
+}
