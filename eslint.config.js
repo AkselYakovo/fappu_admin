cb3f240131b59f5c9ab95c67cb53a9c91cb2fb4c
Axel Jacobo <jacobaxelth@outlook.com>
2025-08-13 09:12:02 -0600

    Create linting file
    Create `eslint.config.js` file. Within this file exist all the linting
    rules that will be enforced to the codebase
    
    See: #34

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default {
+  "rules": {
+    "no-console": "error",
+    "no-unused-vars": "error",
+    "no-redeclare": "error",
+    "no-regex-spaces": "error",
+    "semi": ["warn", "never"],
+    "quotes": ["warn", "double"],
+    "no-unused-vars": "error",
+    "no-trailing-spaces": "error",
+  },
+}
